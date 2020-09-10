23e67e877da9229c5d3904681e15e9f52ab4332e
Dmitry Evdokimov <vlg1evdokimov@gmail.com>
2020-09-10 12:04:18 +0300

    Add ProductManager.java

--- src/main/java/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -6,24 +6,22 @@ import ru.netology.domain.Smartphone;
 import ru.netology.repository.ProductRepository;
 
 public class ProductManager {
-    private Product[] items = new Product[0];
+    private ProductRepository repository;
+
+    public ProductManager(ProductRepository repository) {
+        this.repository = repository;
+    }
 
     public void add(Product item) {
-        int length = items.length + 1;
-        Product[] tmp = new Product[length];
-        System.arraycopy(items, 0, tmp, 0, items.length);
-        int lastIndex = tmp.length - 1;
-        tmp[lastIndex] = item;
-        items = tmp;
+        repository.save(item);
     }
 
     public Product[] searchBy(String text) {
-        ProductRepository repository = new ProductRepository();
         Product[] result = new Product[0];
         for (Product product: repository.findAll()) {
             if (matches(product, text)) {
                 Product[] tmp = new Product[result.length + 1];
-                // используйте System.arraycopy, чтобы скопировать всё из result в tmp
+                System.arraycopy (result,0, tmp,0, result.length);
                 tmp[tmp.length - 1] = product;
                 result = tmp;
             }
